af2f4b7ddd9a85014b0706f62f86078cd8f4850b
xieyunyu09 <xieyunyu@163.com>
2019-01-07 23:08:18 +0800

    登陆授权联调

--- src/main/java/cn/stylefeng/guns/config/util/AuthUtil.java
+++ src/main/java/cn/stylefeng/guns/config/util/AuthUtil.java
@@ -14,8 +14,8 @@ import java.io.IOException;
 public class AuthUtil {
     public static final String OPENID = "OPENID";
     public static final String NICKNAME = "NICKNAME";
-    public static final String APPID = "wx8212f6193ad8e227";
-    public static final String APPSECRET = "3de6a459ef5b8c0cf4f90eb46cb69ea6";
+    public static final String APPID = "wx8212f6198zd8e227";
+    public static final String APPSECRET = "8de6z459ef5b8c0cf4f90eb46cb69ez6";
 //    public static final String SERVER = "http://193.112.92.40/photo";
     public static final String SERVER = "http://ycsshj.oicp.net";
     public static final String TOKEN = "weixi";
