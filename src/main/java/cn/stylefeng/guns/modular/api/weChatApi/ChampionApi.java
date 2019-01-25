050903f465ee10e2420845ed093bb3f1cda366f8
ycsshj <ycsshj@sina.com>
2019-01-25 23:37:20 +0800

    擂主回复作品

--- src/main/java/cn/stylefeng/guns/modular/api/weChatApi/ChampionApi.java
+++ src/main/java/cn/stylefeng/guns/modular/api/weChatApi/ChampionApi.java
@@ -1,9 +1,17 @@
 package cn.stylefeng.guns.modular.api.weChatApi;
 
+import cn.stylefeng.guns.core.util.BUSINESS_MODE_ENUM;
 import cn.stylefeng.guns.modular.champion.service.IChampionService;
 import cn.stylefeng.guns.modular.science.service.IPopularScienceBaseService;
 import cn.stylefeng.guns.modular.system.model.Champion;
 import cn.stylefeng.guns.modular.system.model.PopularScienceBase;
+import cn.stylefeng.guns.modular.system.model.ReplyDetails;
+import cn.stylefeng.guns.modular.system.model.WorksDetails;
+import cn.stylefeng.guns.modular.system.service.IUserService;
+import cn.stylefeng.guns.modular.worksDetail.service.IGiveLikeDetailsService;
+import cn.stylefeng.guns.modular.worksDetail.service.IReplyDetailsService;
+import cn.stylefeng.guns.modular.worksDetail.service.IWorksDetailsService;
+import cn.stylefeng.guns.modular.worksDetail.service.IWorksImgDetailsService;
 import cn.stylefeng.roses.core.base.controller.BaseController;
 import cn.stylefeng.roses.core.reqres.response.ErrorResponseData;
 import cn.stylefeng.roses.core.reqres.response.ResponseData;
@@ -12,6 +20,7 @@ import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.ibatis.reflection.ArrayUtil;
 import org.apache.shiro.crypto.hash.Hash;
@@ -32,6 +41,10 @@ import java.util.*;
 public class ChampionApi extends BaseController {
     @Autowired
     private IChampionService championService;
+    @Autowired
+    private IWorksDetailsService worksDetailsService;
+    @Autowired
+    private IReplyDetailsService replyDetailsService;
     @Autowired
     private IPopularScienceBaseService popularScienceBaseService;
     @GetMapping("/list")
@@ -64,8 +77,38 @@ public class ChampionApi extends BaseController {
                 }
                popularScienceBases = popularScienceBaseService.selectBatchIds(listStrId);
             }
+            List<Map<String,Object>> worksDetailsList = new ArrayList<>();
+            if(champion.getUid()!=null&&champion.getUid()>0){
+                Wrapper<ReplyDetails> wrapper = new EntityWrapper();
+                ReplyDetails replyDetails = new ReplyDetails();
+                replyDetails.setModel(BUSINESS_MODE_ENUM.WORKS_DETAILS.name());
+                replyDetails.setUid(Long.valueOf(champion.getUid()));
+                wrapper.where("model = {0} and uid = {1} and reply_state = 1 ",replyDetails.getModel(),replyDetails.getUid())
+                        .orderBy("create_time",false);
+                List<ReplyDetails> replyDetailss = replyDetailsService.selectList(wrapper);
+                List<String> listDetailId = new ArrayList<>();
+                for(ReplyDetails reply : replyDetailss){
+                    listDetailId.add(reply.getId());
+                }
+                if(listDetailId.size()>0){
+                    Wrapper<WorksDetails> worksDetailsWarp = new EntityWrapper();
+                    WorksDetails worksDetails = new WorksDetails();
+                    worksDetailsWarp.where("details_delete=0 and state=1 ").in("id",listDetailId).orderBy("create_time");
+                    List<WorksDetails> worksDetailss = worksDetailsService.selectList(worksDetailsWarp);
+                    Map<String,Object> mapInfo = null;
+                    for(WorksDetails details : worksDetailss){
+                        mapInfo = new HashedMap();
+                        mapInfo.put("id",details.getId());
+                        mapInfo.put("imgUrl",details.getImgUrl());
+                        mapInfo.put("imgRemark",details.getImgRemark());
+                        mapInfo.put("createTime",details.getCreateTime());
+                        worksDetailsList.add(mapInfo);
+                    }
+                }
+            }
             resultMap.put("champion",champion);
             resultMap.put("popularScienceBases",popularScienceBases);
+            resultMap.put("worksDetailsList",worksDetailsList);
             return new SuccessResponseData(resultMap);
         }catch (Exception e){
             log.error(e.getMessage(),e);
