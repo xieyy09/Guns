b97a90fbe6177d22179d08c9ae595e0dbfccd1ba
xieyy09 <xieyy09@gmail.com>
2019-02-02 14:38:48 +0800

    作品增加属性

--- src/main/java/cn/stylefeng/guns/config/util/AuthUtil.java
+++ src/main/java/cn/stylefeng/guns/config/util/AuthUtil.java
@@ -17,8 +17,8 @@ public class AuthUtil {
     public static final String APPID = "wx70ae73e897e2951c";
     public static final String APPSECRET = "9d0f1cc9ca8e6c188f491dc2ba9583d9";
 //    public static final String SERVER = "http://193.112.92.40/photo";
-    public static final String SERVER = "http://ycsshj.oicp.net/index.html";
-    public static final String REDIRECT_URI = "http://ycsshj.oicp.net/photo/weChatApi/callBack";
+    public static final String SERVER = "http://www.bkqs8.com//index.html";
+    public static final String REDIRECT_URI = "http://www.bkqs8.com/photo/weChatApi/callBack";
     public static final String TOKEN = "weixi";
     public static  String ACCESS_TOKEN = null;
     public static  String TIMESTAMP = "7200";
